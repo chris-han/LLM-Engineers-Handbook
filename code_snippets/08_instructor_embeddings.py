04e92ebe038686e938b264aefa6fe1954e00e04e
chris.han <chrishan2000@hotmail.com>
2025-01-13 09:47:54 +0800

    working

--- code_snippets/08_instructor_embeddings.py
+++ code_snippets/08_instructor_embeddings.py
@@ -6,13 +6,13 @@ from InstructorEmbedding import INSTRUCTOR
 # 3. Install: pip install sentence-transformers==2.2.2 InstructorEmbedding==1.0.1
 # 4. Run the code: python code_snippets/08_instructor_embeddings.py
 
-if __name__ == "__main__":
-    model = INSTRUCTOR("hkunlp/instructor-base")
+# if __name__ == "__main__":
+#     model = INSTRUCTOR("hkunlp/instructor-base")
 
-    sentence = "RAG Fundamentals First"
+#     sentence = "RAG Fundamentals First"
 
-    instruction = "Represent the title of an article about AI:"
+#     instruction = "Represent the title of an article about AI:"
 
-    embeddings = model.encode([[instruction, sentence]])
-    print(embeddings.shape)  # noqa
-    # Output: (1, 768)
+#     embeddings = model.encode([[instruction, sentence]])
+#     print(embeddings.shape)  # noqa
+#     # Output: (1, 768)
